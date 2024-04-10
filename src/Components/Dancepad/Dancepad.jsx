88d7f4cf12c6f1857991634ec6f5b0900315a74d
Ezekiel <evangelista.ezekiel@hotmail.ca>
2024-04-10 09:43:37 -0400

    refactor gamepage

--- src/Components/Dancepad/Dancepad.jsx
+++ src/Components/Dancepad/Dancepad.jsx
@@ -1,12 +1,13 @@
-import "./Dancepad.scss"
+import "./Dancepad.scss";
+import arrow from "../../assets/arrow.svg";
 
-export function Dancepad({onArrowPress}) {
-    return (
-        <div className="dance-pad">
-          <button onClick={() => onArrowPress('up')}>↑</button>
-          <button onClick={() => onArrowPress('down')}>↓</button>
-          <button onClick={() => onArrowPress('left')}>←</button>
-          <button onClick={() => onArrowPress('right')}>→</button>
-        </div>
-      );
+export function Dancepad({ onArrowPress }) {
+  return (
+    <div className="dance-pad">
+      <button onClick={() => onArrowPress('up')}><img src={arrow} alt="Up" /></button>
+      <button onClick={() => onArrowPress('down')}><img src={arrow} alt="Down" /></button>
+      <button onClick={() => onArrowPress('left')}><img src={arrow} alt="Left" /></button>
+      <button onClick={() => onArrowPress('right')}><img src={arrow} alt="Right" /></button>
+    </div>
+  );
 }
--- src/Pages/GamePage/GamePage.jsx
+++ src/Pages/GamePage/GamePage.jsx
@@ -1,20 +1,39 @@
-import "./GamePage.scss"
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import useSound from "use-sound";
 import arrow from "../../assets/arrow.svg";
 import song from "../../assets/BeethovenVirus.mp3";
+import "./GamePage.scss";
 
 export function GamePage() {
     const [score, setScore] = useState(0);
     const [lives, setLives] = useState(5);
+    const [multiplier, setMultiplier] = useState(1);
+    const [combo, setCombo] = useState(0);
     const [isGameRunning, setIsGameRunning] = useState(false);
+    const [play, { stop }] = useSound(song);
 
-    function handleStart() {
+    useEffect(() => {
+        createRow();
+    }, []);
+
+    const createRow = () => {
+        const arrows = ["left", "down", "up", "right"];
+        const newRow = arrows.map((direction, index) => (
+            <div className={`game__play--${direction}-col`} key={index}>
+                <img src={arrow} alt={direction} />
+            </div>
+        ));
+        return newRow;
+    };
+
+    const handleStart = () => {
         setIsGameRunning(true);
-    }
+    };
 
-    if (isGameRunning) {
-        <audio src={song}></audio>
-    }
+    const handleStop = () => {
+        setIsGameRunning(false);
+        stop();
+    };
 
     return (
         <main>
@@ -22,24 +41,31 @@ export function GamePage() {
                 <div className="game__lives">
                     <p>Lives: {lives}</p>
                 </div>
-
-
                 <div className="game__score">
                     <p>Score: {score}</p>
                 </div>
+                <div className="game__multiplier">
+                    <p>Multiplier: {multiplier}x</p>
+                </div>
+                <div className="game__combo">
+                    <p>Combo: {combo}x</p>
+                </div>
             </div>
 
-            <div className="game__play">
-                <div className="game__play--left-col"><img src={arrow} /></div>
-                <div className="game__play--down-col"><img src={arrow} /></div>
-                <div className="game__play--up-col"><img src={arrow} /></div>
-                <div className="game__play--right-col"><img src={arrow} /></div>
-            </div>
 
+            <div className="game__play--generator" id="new-row-generator">
+                {createRow()}
+            </div>
 
             <button className="game__button" onClick={handleStart}>
                 Play!
             </button>
+            <button className="game__button" onClick={handleStop}>
+                Stop!
+            </button>
+            <div className="game__play">
+                {createRow()}
+            </div>
         </main>
     );
 }
